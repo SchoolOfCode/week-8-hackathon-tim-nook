c1d4e9be9e3cd15515e6d148f78c7b7e2263a0c2
Sarah Hunt <shunt11@live.co.uk>
2025-01-30 16:19:42 +0000

    Add form components

--- appreciation-postbox/src/App.jsx
+++ appreciation-postbox/src/App.jsx
@@ -1,8 +1,18 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Form from "./components/Form"
+import DropdownMenu from "./components/Form"
+import NewPostcard from "./components/Form"
+import SelectedPostbox from "./components/Form"
+import Form from "./components/Form"
 
 function App() {
   return <Header></Header>;
+  <Form />;
+  <DropdownMenu/>; 
+  <SelectedPostbox />;
+  <NewPostcard />
+
 }
 
 export default App;
