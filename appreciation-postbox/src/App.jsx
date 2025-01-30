9b085088e123f1d23e192848419e86baac7789ba
Kim Lida <88290393+Kinukeo@users.noreply.github.com>
2025-01-30 16:10:44 +0000

    rendered PostcardContainer in App

--- appreciation-postbox/src/App.jsx
+++ appreciation-postbox/src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import PostcardContainer from "./components/PostcardContainer/PostcardContainer";
 
 function App() {
-  return <Header></Header>;
+  return (
+    <>
+      <Header></Header>
+      <PostcardContainer></PostcardContainer>
+    </>
+  );
 }
 
 export default App;
