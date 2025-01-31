9b2d4e31f272b1836265e4c249144de87389fa4d
Sarah Hunt <shunt11@live.co.uk>
2025-01-31 10:37:35 +0000

    Add css to postcard/postcardcontainer

--- appreciation-postbox/src/components/Postcard/Postcard.jsx
+++ appreciation-postbox/src/components/Postcard/Postcard.jsx
@@ -1,10 +1,14 @@
+import "./Postcard.css";
+
 function Postcard({ postcard }) {
-  return (
-    <>
-      <p>Postcard message: {postcard.content}</p>
-      <p>from... {postcard.sender}</p>
-    </>
-  );
+	return (
+		<>
+			<section className="postcardWrap">
+				<p className="postcardMessage">Postcard message: {postcard.content}</p>
+				<p className="postcardSender">from... {postcard.sender}</p>
+			</section>
+		</>
+	);
 }
 
 export default Postcard;
--- appreciation-postbox/src/components/PostcardContainer/PostcardContainer.jsx
+++ appreciation-postbox/src/components/PostcardContainer/PostcardContainer.jsx
@@ -1,17 +1,31 @@
 import Postcard from "../Postcard/Postcard";
+import "./PostcardContainer.css";
 
 function PostcardContainer({ postcards }) {
-  if (postcards.length === 0) {
-    return <p>Select a Postbox to see Postcards.</p>;
-  }
+	if (postcards.length === 0) {
+		return <p>Select a Postbox to see Postcards.</p>;
+	}
 
-  return (
-    <div>
-      {postcards.map((postcard) => (
-        <Postcard key={postcard.postcardId} postcard={postcard} />
-      ))}
-    </div>
-  );
+	const borderColorClasses = [
+		"borderColor1",
+		"borderColor2",
+		"borderColor3",
+		"borderColor4",
+	];
+
+	return (
+		<div className="PostcardContainerWrapper">
+			<div className="PostcardContainer">
+				{postcards.map((postcard, index) => (
+					<Postcard
+						key={postcard.postcardId}
+						postcard={postcard}
+						className={borderColorClasses[index % 4]}
+					/>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default PostcardContainer;
